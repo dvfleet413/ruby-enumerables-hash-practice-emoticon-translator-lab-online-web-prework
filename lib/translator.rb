4469729b89ff7893690ae9cfaef62ce75376fe5b
David Van Fleet <dvfleet413@gmail.com>
2019-12-22 02:20:11 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,8 +4,13 @@ require "yaml"
 
 def load_library(path)
   # code goes here
+  result = {
+    :get_meaning => [],
+    :get_emoticon => []
+  }
   emoticons = YAML.load_file(path)
   pp emoticons
+  result
 end
 
 def get_japanese_emoticon
